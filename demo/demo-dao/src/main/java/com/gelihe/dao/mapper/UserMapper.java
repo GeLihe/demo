93019fcf254b65ea1568a8a563b96f8aea7db6f5
gelh <gelh@esensoft.com>
2019-10-06 17:01:28 +0800

    自动生成mybatis代码

---
+++ demo/demo-dao/src/main/java/com/gelihe/dao/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.gelihe.dao.mapper;
+
+import com.gelihe.dao.bean.User;
+import com.gelihe.dao.bean.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+}
